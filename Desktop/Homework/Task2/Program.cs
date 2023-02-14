bfa0423cf551775ec11988d3f2402d2b8a05dd52
igor <ftain44@mail.ru>
2023-02-14 04:00:34 +0300

    Теперь все правильно

---
+++ Desktop/Homework/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//№2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число 1: ");
+Console.WriteLine("Введите число 2: ");
+//Ожидаем ввод
+string? string1 = Console.ReadLine ();
+string? string2 = Console.ReadLine ();
+//Проверка на ноль
+if (string1 !=null && string2 !=null)
+{
+    int num1 = int.Parse(string1);
+    int num2 = int.Parse(string2);
+    if (num1 > num2) { 
+        Console.WriteLine("max = " + num1 + " min = " + num2);
+    } else { 
+        Console.WriteLine("max = " + num2 + " min = " + num1);
+    }
+}
